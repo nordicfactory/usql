f1c433dc4231a0fe632b0ccfe6619897cc22b447
Victor Blomqvist <victor.blomqvist@bannerflow.com>
2017-10-20 10:02:39 +0200

    Basic version of a AvroOutputter

---
+++ Examples/DataFormats/Microsoft.Analytics.Samples.Formats.Tests/AvroOutputterTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Samples.Formats.ApacheAvro;
+using Microsoft.Analytics.UnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Analytics.Samples.Formats.Tests
+{
+    
+    [TestClass]
+    public class AvroOutputterTest
+    {
+        [TestMethod]
+        public void TestF()
+        {
+            var foo = new USqlColumn<string>("Type");
+            var columns = new List<IColumn> { foo };
+            var schema = new USqlSchema(columns);
+            var row1 = new USqlRow(schema, new object[]{"1111"});
+            var row2 = new USqlRow(schema, new object[]{"2222"});
+            
+            using (var f = File.Create(@"C:\code\notebooks\data\avro\tmp-avrooutputter\avrooutputtertest1.avro"))
+            {
+                var sw = new USqlStreamWriter(f);
+                
+                var outputter = new AvroOutputter(@"{""type"":""record"",""name"":""Microsoft.Streaming.Avro.GenericFromIRecord0"",""fields"":[{""name"":""Type"",""type"":[""null"",""string""]}]}");
+                outputter.Output(row1, sw);
+                outputter.Output(row2, sw);
+                outputter.Close();
+                sw.Close();
+            } 
+
+        }
+    }
+}
---
+++ Examples/DataFormats/Microsoft.Analytics.Samples.Formats/Avro/AvroOutputter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avro;
+using Avro.File;
+using Avro.Generic;
+using Microsoft.Analytics.Interfaces;
+
+namespace Microsoft.Analytics.Samples.Formats.ApacheAvro
+{
+    [SqlUserDefinedOutputter(AtomicFileProcessing = true)]
+    public class AvroOutputter : IOutputter
+    {
+        private IFileWriter<GenericRecord> _fileWriter;
+        private readonly string _avroSchema;
+        private RecordSchema _avSchema;
+
+        public AvroOutputter(string avroSchema)
+        {
+            _avroSchema = avroSchema;
+        }
+
+        public override void Output(IRow input, IUnstructuredWriter output)
+        {
+            if (_fileWriter == null)
+            {
+                _avSchema = Schema.Parse(_avroSchema) as RecordSchema;
+                var writer = new GenericDatumWriter<GenericRecord>(_avSchema);
+                _fileWriter = DataFileWriter<GenericRecord>.OpenWriter(writer, output.BaseStream);
+            }
+            
+            var record = new GenericRecord(_avSchema);
+            
+            foreach (var x in input.Schema)
+            {
+                record.Add(x.Name, input.Get<object>(x.Name));
+            }
+            
+            _fileWriter.Append(record);
+        }
+
+        public override void Close()
+        {
+            if (_fileWriter == null) return;
+            try
+            {
+                _fileWriter.Dispose();
+            }
+            catch (Exception)
+            {
+                //Ignore this exception, since it will always(?) happen but we still want to make sure the writer is disposed.
+                //Debug.WriteLine($"Exception in writer dispose. {e.Message} {e.StackTrace}");
+            }
+        }
+    }
+}
